b0376d1cff5fac18284edc53446b8810c73cd7ec
Khushi <khushimehta0206@gmail.com>
2025-12-21 16:14:09 +0530

    Invoke Configure Program.cs as entry point for conditional demos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConditionalStatement;
+
+Console.WriteLine("Hello, World!");
+HeightCategory.HeightMain(); 
+LargestOfThree.LargestOfThreeMain();
+LeapYearChecker.LeapYearMain();
+QuadraticEquation.FindRoots();
+AdmissionEligibility.AdmissionEligibilityMain();
+ElectricityBill.CalculateElectricityBill();
+VowelOrConsonant.VowelOrConsonantMain();
+TriangleType.CheckTriangleType();
+QuadrantFinder.FindQuadrant();
+GradeDescription.GradeDescriptionMain();
+ValidDateCheck.DateChecker();
+ATMWithdrawal.ATMWithdrawalMain();
+ProfitLoss.ProfitLossMain();
+RockPaperScissors.RockPaperScissorsMain();
+SampleCalculator.SampleCalculatorMain();
