a1ea491e3ebdf1d972cc7172fd1cf9a8cb336e9f
Khushi <khushimehta0206@gmail.com>
2025-12-22 21:53:18 +0530

    Add main entry point for programs

---
+++ LoopExercises/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LoopStatements;
+
+Console.WriteLine("Hello, World!");
+FibonacciSeries.PrintFibonaaci();
+PrimeNumber.CheckPrime();
+ArmstrongNumber.CheckArmstrong();
+PalindromeCheck.CheckPalindrome();
+GCDAndLCM.FindGcdAndLcm();
+PascalTriangle.PrintPascalTriangle();
+BinaryToDecimal.BinaryToDecimalMain();
+DiamondPattern.PrintDiamondPattern();
+Factorial.FindFactorial();
+GussessingGame.GussessingGameMain();
+SumOfDigits.SumOfDigitsMain();
+ContinueUsage.PrintExceptMultipleOf3();
+MenuSystem.MenuSystemMain();
+StrongNumber.StrongNumberMain();
+SearchWithGoto.SearchWithGotoMain();
