d5ae82e3f9868077784a484f2bcfd8f33d86070c
Khushi <khushimehta0206@gmail.com>
2025-12-22 22:40:56 +0530

    Add continue usage  program

---
+++ LoopExercises/ContinueUsage.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace LoopExercises;
+
+public class ContinueUsage
+{
+    public static void PrintExceptMultipleOf3()
+    {
+        for(int i = 1; i <= 50; i++)
+        {
+            if (i % 3 == 0)
+            {
+                continue;
+            }
+            Console.Write(i+" ");
+        }
+    }
+}
