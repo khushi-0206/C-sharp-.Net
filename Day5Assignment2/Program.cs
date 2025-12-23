4c1883fcb29a8b8ff836f7ce764c7445de923862
Khushi <khushimehta0206@gmail.com>
2025-12-23 16:14:07 +0530

    Add Program entry file Program.cs

---
+++ Day5Assignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day6Assignment2;
+try
+{
+    
+    Cake cake = new Cake();
+
+    Console.Write("Enter the Flavour: ");
+    cake.Flavour = Console.ReadLine();
+
+    Console.Write("Enter the quantity in kg: ");
+    cake.QuatityInKg = Int32.Parse(Console.ReadLine());
+
+    Console.Write("Enter the Price Per Kg: ");
+    cake.PricePerKg = double.Parse(Console.ReadLine());
+
+    cake.CakeOrder();
+    Console.WriteLine("Cake order was successful");
+    Console.WriteLine("Price after discount is {0}",cake.DiscountedPrice);
+
+}
+catch(InvalidFlavourException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch(InvalidQuatityException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
