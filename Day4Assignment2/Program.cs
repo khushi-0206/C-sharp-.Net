765f8e7ab34a6d7b1b3ed70603c4513d0db84388
Khushi <khushimehta0206@gmail.com>
2025-12-24 09:08:24 +0530

    Add entry file for program

---
+++ Day4Assignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+PassengerTicket ptObj = new PassengerTicket();
+Console.Write("Enter Passenger Id: ");
+ptObj.PassengerId = Int32.Parse(Console.ReadLine());
+
+Console.Write("Enter Passenger Name: ");
+ptObj.PassengerName = Console.ReadLine();
+
+Console.Write("Enter Age: ");
+ptObj.Age = Int32.Parse(Console.ReadLine());
+
+if(!ptObj.ValidateAge()) return;
+
+Console.Write("Enter Base Fare: ");
+ptObj.BaseFare = double.Parse(Console.ReadLine());
+
+Console.WriteLine("-----Choose Travel Type-------");
+Console.WriteLine("1. Bus\t2.Train\t3.Flight");
+Console.Write("Enter choice: ");
+ptObj.TravelType = Int32.Parse(Console.ReadLine());
+
+ptObj.SelectTravelType();
+
+Console.Write("1.Yes\t2.No\nIs GovernmentEmployee: ");
+ptObj.IsGovernmentEmployee = Int32.Parse(Console.ReadLine());
+
+ptObj.DiscountCalculation();
+ptObj.FareCalculationAfterDiscount();
+ptObj.BookingStatusCheck();
+ptObj.DisplayDetails();
