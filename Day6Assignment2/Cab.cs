40ca3eedc0cfc264e6600d43e1ecbbcb571839c6
Khushi <khushimehta0206@gmail.com>
2025-12-24 15:57:00 +0530

    Implement parent entity class

---
+++ Day6Assignment2/Cab.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Day6Assignment2;
+
+public class Cab
+{
+    public string BookingId{get; set;}
+    public string CabType{get; set;}
+    public double Distance{get; set;}
+    public int WatingTime{get; set;}
+
+    
+}
