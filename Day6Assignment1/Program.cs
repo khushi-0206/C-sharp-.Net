df482351028894388e69b5f6f916b4daba39168b
Khushi <khushimehta0206@gmail.com>
2025-12-24 15:08:21 +0530

    Add entry file for program

---
+++ Day6Assignment1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day6Assignment1;
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Enter the title: ");
+string title = Console.ReadLine();
+
+Console.Write("Enter the author: ");
+string author = Console.ReadLine();
+
+Console.Write("Enter the number of pages: ");
+int numOfPages = Int32.Parse(Console.ReadLine());
+
+Console.Write("Enter the due date {dd/mm/yyyy}: ");
+DateTime dueDate = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Enter the return date {dd/mm/yyyy}: ");
+DateTime returnDate = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Enter the days to read: {dd/mm/yyyy}");
+int daysToread = Int32.Parse(Console.ReadLine());
+
+Console.Write("Enter the daily late fee rate: ");
+int dailyLateFeeRate = Int32.Parse(Console.ReadLine());
+
+Book bookObj = new Book(title,author,numOfPages,dueDate,returnDate);
+
+double averagePageReadPerDay = bookObj.AveragePagesReadPerDay(daysToread);
+double lateFee = bookObj.CalculateLateFee(dailyLateFeeRate);
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Average Pages Per Day: {0}",averagePageReadPerDay);
+Console.WriteLine("Late Fee: {0}",lateFee);
+
+Console.ForegroundColor = ConsoleColor.White;
