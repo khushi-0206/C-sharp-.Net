80f96ee1e989f39399f22111bf1230848a2bb55c
Khushi <khushimehta0206@gmail.com>
2025-12-24 15:59:24 +0530

    Add entry class for program

---
+++ Day6Assignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day6Assignment2;
+
+CabDetails cabDetailsObj = new CabDetails();
+Console.WriteLine("Enter the booking id: ");
+cabDetailsObj.BookingId = Console.ReadLine();
+
+bool validateId = cabDetailsObj.ValidateBookingId();
+if (!validateId)
+{
+    Console.WriteLine("Invalis Booking id");
+    return;
+}
+
+Console.WriteLine("Enter Cab type: ");
+cabDetailsObj.CabType = Console.ReadLine();
+
+Console.WriteLine("Enter the distance: ");
+cabDetailsObj.Distance = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the wating time in minutes: ");
+cabDetailsObj.WatingTime= Int32.Parse(Console.ReadLine());
+
+double fareAmount = cabDetailsObj.CalculateFareAmount();
+
+Console.WriteLine("The fare Amount is: {0:F2}", fareAmount);
