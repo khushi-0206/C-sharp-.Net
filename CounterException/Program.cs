54321ad1d968a1f5dd961e4be659a68f7cf8180d
Khushi <khushimehta0206@gmail.com>
2026-01-03 13:38:07 +0530

    Add Program.cs class

---
+++ CounterException/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter size: ");
+int size = Int32.Parse(Console.ReadLine());
+
+int[] data = new int[size];
+Console.WriteLine("Enter elements: ");
+for(int i = 0; i < size; i++)
+{
+    data[i] = Int32.Parse(Console.ReadLine());
+}
+Counter cObj = null;
+try
+{
+    cObj = new Counter(data);
+    string res = cObj.GetCount();
+    Console.WriteLine(res);
+}
+catch(ExceptionZero ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch(ExceptionOne ex)
+{
+    Console.WriteLine(ex.Message);
+}
