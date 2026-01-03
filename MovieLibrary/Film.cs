6517ad367d905106067dd57bf2f5e0c9a104cbf9
Khushi <khushimehta0206@gmail.com>
2026-01-03 12:56:17 +0530

    Implement Film entity class

---
+++ MovieLibrary/Film.cs
@@ -0,0 +1,14 @@
+public class Film:IFilm
+{
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public int Year { get; set; }
+
+    public Film(string title, string director, int year)
+    {
+        Title = title;
+        Director = director;
+        Year = year;
+    }
+
+}
