aa25cadbb102821f3f368720fcad4b9fa0af5473
Khushi <khushimehta0206@gmail.com>
2026-01-13 15:43:47 +0530

    Implement entry point class program

---
+++ PhoneCallSubscription/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        PhoneCall pc = new PhoneCall();
+
+        pc.MakePhoneCall(true);
+        System.Console.WriteLine(pc.Message);
+
+        pc.MakePhoneCall(false);
+        System.Console.WriteLine(pc.Message);
+    }
+}
