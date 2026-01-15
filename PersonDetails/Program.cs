032ff93522d34778fc2a4aa258ce49c8741e41b9
Khushi <khushimehta0206@gmail.com>
2026-01-15 15:31:13 +0530

    Implement entry class program

---
+++ PersonDetails/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+IList<Person> p = new List<Person>();
+p.Add(new Person{Name = "Aarya", Address = "A2101", Age = 69});
+p.Add(new Person {Name = "Daniel", Address = "D104",Age = 40});
+p.Add(new Person {Name = "Ira", Address = "H801",Age = 25});
+p.Add(new Person {Name = "Jennifer", Address = "I1704",Age = 33});
+
+PersonImplementation piObj = new PersonImplementation();
+System.Console.WriteLine(piObj.GetName(p));
+System.Console.WriteLine(piObj.Average(p));
+System.Console.WriteLine(piObj.Max(p));
