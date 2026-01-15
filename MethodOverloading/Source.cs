25ca66bb1559840871198b6d881a4e743511dafb
Khushi <khushimehta0206@gmail.com>
2026-01-15 15:50:11 +0530

    Implement BL class to show method overloading

---
+++ MethodOverloading/Source.cs
@@ -0,0 +1,11 @@
+class Source
+{
+    public int Add(int a, int b, int c)
+    {
+        return a+b+c;
+    }
+    public double Add(double a, double b, double c)
+    {
+        return a+b+c;
+    }
+}
