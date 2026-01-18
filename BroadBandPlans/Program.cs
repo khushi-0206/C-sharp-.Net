e9f43aba0d9c62d5a6eec117a19d850e2cb920bd
Khushi <khushimehta0206@gmail.com>
2026-01-18 18:55:21 +0530

    Implement entry class

---
+++ BroadBandPlans/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        var plans = new List<IBroadbandPlan>()
+        {
+            new Black(true, 50),
+            new Black(false, 10),
+            new Gold(true, 30),
+            new Black(true, 20),
+            new Gold(false, 20)
+        };
+
+        var subscriptionPlans = new SubscribePlan(plans);
+        var result = subscriptionPlans.GetSubscriptionPlan();
+
+        foreach (var item in result)
+        {
+            Console.WriteLine($"{item.Item1}, {item.Item2}");
+        }
+    }
+}
