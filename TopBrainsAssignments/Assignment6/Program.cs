85741d4ecdc46549cb48e0e4f1e0971f51579151
Khushi <khushimehta0206@gmail.com>
2026-01-19 12:38:53 +0530

    Display Height

---
+++ TopBrainsAssignments/Assignment6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter height (in cm): ");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        string category = HeightCategory(height);
+        Console.WriteLine(category);
+    }
+    public static string HeightCategory(int height)
+    {
+        if (height < 150)
+        {
+            return "Short";
+        }
+        else if(height >=150 && height < 180)
+        {
+            return "Average";
+        }
+        else
+        {
+            return "Tall";
+        }
+    }
+}
