ed5bd15f6280e4323d038aa3ff81929d86d7a1da
Khushi <khushimehta0206@gmail.com>
2026-01-19 11:33:42 +0530

    Largest Number

---
+++ TopBrainsAssignments/Assignment5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int largestNumber = FindLargestOfThree(num1,num2,num3);
+
+        Console.WriteLine($"Largest of {num1}, {num2} and {num3} is {largestNumber}");
+    }
+    public static int FindLargestOfThree(int a, int b, int c)
+    {
+        if(a>b && a > c)
+        {
+            return a;
+        }
+        else if(b>a && b > c)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+}
