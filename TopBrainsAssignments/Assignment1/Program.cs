42a0036aecd7e2e0f2f45548e2d551c90a81081a
Khushi <khushimehta0206@gmail.com>
2026-01-19 10:16:10 +0530

    Mahirl And Alphabet And Vowel

---
+++ TopBrainsAssignments/Assignment1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Text;
+
+class Program
+{
+    public static string StringAfterManipulation(string str1, string str2)
+    {
+        HashSet<char> set = new HashSet<char>();
+        foreach(char c in str2)
+        {
+            set.Add(c);
+        }
+
+        StringBuilder filtered = new StringBuilder();
+        foreach(char c in str1)
+        {
+            if(IsConsonant(c) && set.Contains(c))
+            {
+                continue;
+            }
+            else
+            {
+                filtered.Append(c);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < filtered.Length; i++)
+        {
+            if(i==0 || filtered[i] != filtered[i - 1])
+            {
+                result.Append(filtered[i]);
+            }
+        }
+        return result.ToString();
+    }
+    private static bool IsConsonant(char c)
+    {
+        if (!char.IsLetter(c))
+        {
+            return false;
+        }
+
+        return !"aeiou".Contains(c);
+    }
+    public static void Main()
+    {
+        Console.Write("Enter first String: ");
+        string str1 = Console.ReadLine().ToLower();
+
+        Console.Write("Enter second string: ");
+        string str2 = Console.ReadLine().ToLower();
+
+        string result = StringAfterManipulation(str1,str2);
+        Console.WriteLine("String after removal: "+result);
+
+        
+    }
+}
