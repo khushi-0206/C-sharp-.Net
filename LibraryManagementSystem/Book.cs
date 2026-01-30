ed19ad2ca9baf4746a545655d77bd124b0c99a58
Khushi <khushimehta0206@gmail.com>
2026-01-31 00:39:05 +0530

    Implement Book entity class

---
+++ LibraryManagementSystem/Book.cs
@@ -0,0 +1,11 @@
+namespace LibraryManagement.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public double Price { get; set; }
+    }
+}
