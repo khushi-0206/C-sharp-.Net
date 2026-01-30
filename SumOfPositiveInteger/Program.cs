869cbd1092dbb2b289c4be35c50ca94a97fcf17b
Khushi <khushimehta0206@gmail.com>
2026-01-31 00:13:13 +0530

    project and solution file added

---
+++ SumOfPositiveInteger/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter size: ");
+        int n = Int32.Parse(Console.ReadLine());
+
+        int[] nums = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            nums[i] = Int32.Parse(Console.ReadLine());
+        }
+
+        int result = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] < 0)
+            {
+                continue;
+            }
+            else if (nums[i] == 0)
+            {
+                break;
+            }
+            else
+            {
+                result += nums[i];
+            }
+        }
+        Console.WriteLine("Sum: "+result);
+    }
+}
