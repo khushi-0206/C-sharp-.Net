59438b374c6432211073123f2480aee9c53e96c6
Khushi <khushimehta0206@gmail.com>
2026-02-02 19:54:23 +0530

    Remove consecutive Character

---
+++ TopBrainsAssignments/Assignment10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter string: ");
+        string input = Console.ReadLine();
+        string res = RemoveConsecutiveCharacter(input);
+        Console.WriteLine(res);
+    }
+    public static string RemoveConsecutiveCharacter(string input)
+    {
+        StringBuilder sb = new StringBuilder();
+        char c = input[0];
+        for(int i = 1;i<input.Length;i++)
+        {
+            if(c == input[i])
+            {
+                continue;
+            }
+            else
+            {
+                sb.Append(c);
+                c = input[i];
+            }
+        }
+        sb.Append(input[input.Length-1]);
+        return sb.ToString();
+    }
+}
