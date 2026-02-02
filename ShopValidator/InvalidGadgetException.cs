2be25a28754b2bdf72082333a1a184b8d1449dcf
Khushi <khushimehta0206@gmail.com>
2026-02-02 14:21:54 +0530

    Add custom exception file

---
+++ ShopValidator/InvalidGadgetException.cs
@@ -0,0 +1,11 @@
+class InvalidGadgetExcetion : Exception
+{
+    public InvalidGadgetExcetion()
+    {
+        
+    }
+    public InvalidGadgetExcetion(string message) : base(message)
+    {
+        
+    }
+}
