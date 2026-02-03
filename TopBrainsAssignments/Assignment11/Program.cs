b66cbf2a624fffa196778a55d847ab0e13f7c82c
Khushi <khushimehta0206@gmail.com>
2026-02-03 16:55:50 +0530

    Feet conversion

---
+++ TopBrainsAssignments/Assignment11/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static double FeetToCentimeters(int feet)
+    {
+        double centimeters = feet * 30.48;
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
+    public static void Main()
+    {
+        System.Console.WriteLine("Enter value in feet: ");
+        int feet = Convert.ToInt32(Console.ReadLine());
+        double result = FeetToCentimeters(feet);
+        Console.WriteLine(result); 
+    }
+}
