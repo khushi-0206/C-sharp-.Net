492aa749bb8d82661747ec7896e7c66bd9795608
Khushi <khushimehta0206@gmail.com>
2026-02-07 15:46:24 +0530

    Implememt OrderException class

---
+++ Assignment_7_Feb/MiniOrderSystem/OrderException.cs
@@ -0,0 +1,11 @@
+public class OrderException : Exception
+{
+    public OrderException()
+    {
+        
+    }
+    public OrderException(string message) : base(message)
+    {
+        
+    }
+}
