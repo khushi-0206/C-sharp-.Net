f315f406e15dcd2d9b6d6c250fc555df20538951
Khushi <khushimehta0206@gmail.com>
2026-02-07 15:47:42 +0530

    Implememt Payment failed exception class

---
+++ Assignment_7_Feb/MiniOrderSystem/PaymentFailedException.cs
@@ -0,0 +1,11 @@
+public class PaymentFailedException : Exception
+{
+    public PaymentFailedException()
+    {
+        
+    }
+    public PaymentFailedException(string message):base(message)
+    {
+        
+    }
+}
