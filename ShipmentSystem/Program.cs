40df54031730290bf7eb0204c25fcff5e2d895b6
Khushi <khushimehta0206@gmail.com>
2026-02-14 11:05:04 +0530

    Implement program class

---
+++ ShipmentSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Nodes;
+
+class Program
+{
+    public static void Main()
+    {
+        ShipmentDetails sObj = new ShipmentDetails();
+        System.Console.Write("Input ID: ");
+        sObj.ShipmentCode = Console.ReadLine();
+
+
+        if (sObj.ValidateShipmentCode(sObj.ShipmentCode))
+        {
+            System.Console.Write("Mode: ");
+            sObj.TransportMode = Console.ReadLine();
+
+            System.Console.Write("Weight: ");
+            sObj.weight = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.Write("Storage: ");
+            sObj.StorageDays = Convert.ToInt32(Console.ReadLine());
+
+            double totalCost = sObj.CalculateTotalCost();
+            System.Console.WriteLine($"The total shipping cost is {totalCost:F2}");
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid Shipment code");
+        }
+
+
+    }
+}
