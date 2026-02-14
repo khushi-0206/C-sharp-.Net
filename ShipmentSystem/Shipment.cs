3fc13219a2f2ca89019e75c792bd74f34193b043
Khushi <khushimehta0206@gmail.com>
2026-02-14 11:05:21 +0530

    Implement Shipment entity class

---
+++ ShipmentSystem/Shipment.cs
@@ -0,0 +1,8 @@
+public class Shipment
+{
+    public string ShipmentCode{get;set;}
+    public string TransportMode{get;set;}
+    public double weight{get;set;}
+    public int StorageDays{get;set;}
+    
+}
