6372b580b962a28d36bce29bdb2c505face3f2d7
Khushi <khushimehta0206@gmail.com>
2026-02-14 12:51:28 +0530

    Implement Book entity class

---
+++ BookStoreApplication/Book.cs
@@ -0,0 +1,8 @@
+public class Book
+{
+    public string BookId{get;set;}
+    public string Title{get;set;}
+    public string Author{get;set;}
+    public int Price{get;set;}
+    public int Stock{get;set;}
+}
