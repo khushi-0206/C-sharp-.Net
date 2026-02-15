b2b98a023e26564def4a5353ac98a739215dc83c
Khushi <khushimehta0206@gmail.com>
2026-02-15 19:06:22 +0530

    Implement Program class

---
+++ EmployeeAnalyzer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+class Employee
+{
+    public string EmployeeCode{get;set;}
+    public string Name{get;set;}
+    public int Salary{get;set;}
+}
+class Program
+{
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        List<Employee> employees = new List<Employee>();
+
+        for(int i = 0; i < n; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            employees.Add(new Employee{EmployeeCode = inputs[0], Name = inputs[1], Salary = Convert.ToInt32(inputs[2])});
+        }
+
+        List<Employee> res = employees.Where(e => Regex.IsMatch(e.EmployeeCode,@"^[A-Z]{2}\d{3}[a-z]$"))
+                            .Where(e => e.Salary >= 30000)
+                            .OrderByDescending(e => e.Salary).ThenBy(e => e.Name)
+                            .ToList();
+        if (res.Count == 0)
+        {
+            Console.WriteLine("No Valid Employees");
+        }
+        else
+        {
+            foreach(Employee emp in res)
+            {
+                Console.WriteLine(emp.Name);
+            }
+        }
+    }
+}
