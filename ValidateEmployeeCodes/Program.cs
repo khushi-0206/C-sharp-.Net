fecb0d8e0e7191661966f8cab1126cae03004417
Khushi <khushimehta0206@gmail.com>
2026-02-15 17:24:54 +0530

    Implement program class

---
+++ ValidateEmployeeCodes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        List<string> codes = new List<string>();
+        for(int i = 0; i < n; i++)
+        {
+            codes.Add(Console.ReadLine());
+        }
+
+        List<int> res = ValidateCodes(codes);
+        foreach(int r in res)
+        {
+            Console.WriteLine(r);
+        }
+    }
+    public static List<int> ValidateCodes(List<string> codes)
+    {
+        List<int> res = new List<int>();
+        foreach(string code in codes)
+        {
+            if (Regex.IsMatch(code, @"^[A-Z]{2}\d{3}[a-z]$"))
+            {
+                res.Add(1);
+            }
+            else
+            {
+                res.Add(0);
+            }
+        }
+        return res;
+    }
+}
