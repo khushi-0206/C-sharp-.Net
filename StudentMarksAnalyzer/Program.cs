a92ff02e23fdfc191ede997177d3a298f226dee5
Khushi <khushimehta0206@gmail.com>
2026-02-15 17:48:21 +0530

    Implement program class

---
+++ StudentMarksAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        List<Student> students = new List<Student>();
+        for(int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            string[] inputArr = input.Split(" ");
+            students.Add(new Student{Name = inputArr[0], Marks = Convert.ToInt32(inputArr[1])});
+        }
+        int highestMarks = students.Max(s => s.Marks);
+        string studentWithHighestMarks = students.Where(s => s.Marks == highestMarks).Select(s => s.Name).FirstOrDefault();
+        Console.WriteLine(studentWithHighestMarks);
+    }
+}
