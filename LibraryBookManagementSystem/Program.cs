efcc6dcb4fdff05a087b6026a034f6547236d1d3
Khushi <khushimehta0206@gmail.com>
2026-02-16 23:12:35 +0530

    Implement Program class

---
+++ LibraryBookManagementSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Catalog<Book> library = new Catalog<Book>();
+        Book b1 = new Book
+        {
+            ISBN = "978-3-16-148410-0", 
+            Title = "C# Programming", 
+            Author = "John Sharp", 
+            Genre = "Programming"
+ 
+        };
+
+        library.AddItem(b1);
+        var programmingBooks = library["Programming"];
+        Console.WriteLine(programmingBooks.Count);
+
+        var johnsBooks = library.FindBooks(b => b.Author.Contains("John"));
+        Console.WriteLine(johnsBooks.Count());
+
+    }
+}
