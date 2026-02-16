c6017c85bb0e359d3b51aadd692279b9fd3b39ee
Khushi <khushimehta0206@gmail.com>
2026-02-16 23:12:13 +0530

    Implement Book class

---
+++ LibraryBookManagementSystem/Book.cs
@@ -0,0 +1,8 @@
+public class Book
+{
+    public string ISBN{get;set;}
+    public string Title{get;set;}
+    public string Author{get;set;}
+    public string Genre{get;set;}
+    public bool IsAvailable{get;set;}
+}
