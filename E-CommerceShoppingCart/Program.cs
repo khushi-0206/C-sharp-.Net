1de0dc2a01a809a609c7b94453796ef228e0044b
Khushi <khushimehta0206@gmail.com>
2026-02-17 10:17:21 +0530

    Implement program class

---
+++ E-CommerceShoppingCart/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        ShoppingCart<Electronics> cart = new ShoppingCart<Electronics>();
+        cart.AddToCart(new Electronics{Id = 1, Name = "Laptop",Price = 999.99},1);
+        cart.AddToCart(new Electronics{Id = 2, Name = "Mouse",Price = 29.99},2);
+
+        double total = cart.CalculateTotal((product,price) => price > 100 ? price *0.9:price);
+        Console.WriteLine($"Total: {total:F2}");
+
+        var topItems = cart.GetTopExpensiveItems(1);
+        foreach(var items in topItems)
+        {
+            Console.WriteLine(items.Name);
+        }
+    }
+}
