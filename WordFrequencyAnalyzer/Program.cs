e4ad0a37ed0ed30ad61d8504bae63f37d36bf334
Khushi <khushimehta0206@gmail.com>
2026-02-17 12:43:22 +0530

    Implement program class

---
+++ WordFrequencyAnalyzer/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string[] str = Console.ReadLine()
+        .Replace(".","")
+        .Replace("?","")
+        .Replace("!","")
+        .Replace(",","")
+        .ToLower().Split(" ");
+        
+        Dictionary<string,int> freq = new Dictionary<string, int>();
+        foreach(string word in str)
+        {
+            if (freq.ContainsKey(word))
+            {
+                freq[word] = freq[word] + 1;
+            }
+            else
+            {
+                freq.Add(word,1);
+            }
+        }
+        var removed = freq.Where(i => i.Value != 1).Select(i => i).ToDictionary();
+        var sorted = removed.OrderByDescending(i => i.Value).ThenBy(i => i.Key).ToDictionary();
+
+        if(sorted.Count == 0)
+        {
+            Console.WriteLine("No repeated words");
+            return;
+        }
+        foreach(var item in sorted)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
+    }
+}
