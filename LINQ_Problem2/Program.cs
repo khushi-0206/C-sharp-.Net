dce6f5bb7d7f0e01fb66b82b75c2bb43e9954bc9
Khushi <khushimehta0206@gmail.com>
2026-02-22 21:33:48 +0530

    Implement program file

---
+++ LINQ_Problem2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Input list
+        List<string> products = new List<string>()
+        {
+            "Pen", "Book", "Pen", "Pencil", "Book"
+        };
+
+        // LINQ to find duplicates
+        List<string> duplicates = products.GroupBy(p => p).Where(g => g.Count() > 1)
+                                  .Select(g => g.Key).ToList();
+        Console.WriteLine("Duplicate Products:");
+        foreach (var item in duplicates)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
