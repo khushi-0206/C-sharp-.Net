964efcd474e0ebba296efacc977d55491b2022f8
Khushi <khushimehta0206@gmail.com>
2026-03-19 15:23:17 +0530

    Order service

---
+++ RabbitMQ/RabbitMQDemo/RabbitMQDemo/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQDemo.Model;
+using System.Text;
+using System.Text.Json;
+
+namespace ProducerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<string> CreateOrder([FromBody] Order order)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync();
+
+            // Queue for Order Service → Payment Service
+            await channel.QueueDeclareAsync(
+                queue: "order_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var message = JsonSerializer.Serialize(order);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(
+                exchange: "",
+                routingKey: "order_queue",
+                body: body);
+
+            return "Order Created & Sent for Payment!";
+        }
+    }
+}
---
+++ RabbitMQ/RabbitMQDemo/RabbitMQDemo/Model/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQDemo.Model
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Product { get; set; }
+        public double Amount { get; set; }
+    }
+}
